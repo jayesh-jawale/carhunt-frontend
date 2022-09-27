3dea1be4b59712212290dca02a3a1a00f8fc52f5
jayesh-jawale <jawalejayesh123@gmail.com>
2022-09-28 00:35:16 +0530

    payment screen

---
+++ src/components/payment.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Container, Form, Button, Col } from "react-bootstrap";
+
+import { useHistory } from "react-router-dom";
+
+import { CheckoutSteps } from "../pages/checkoutStepsPage";
+
+export function Payment() {
+  const history = useHistory();
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    localStorage.setItem("paymentMethod", JSON.stringify(paymentMethod));
+    history.push("/placeorder");
+  };
+
+  return (
+    <div>
+      <Container>
+        <CheckoutSteps step1 step2 />
+        <h1>Payment Method</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group>
+            <Form.Label as="legend">Select Method</Form.Label>
+            <Col>
+              <Form.Check
+                type="radio"
+                label="PayPal or Credit Card"
+                id="PayPal"
+                name="paymentMethod"
+                value="PayPal"
+                checked
+                onChange={(e) => setPaymentMethod(e.target.value)}
+              ></Form.Check>
+              {/* <Form.Check
+              type='radio'
+              label='Stripe'
+              id='Stripe'
+              name='paymentMethod'
+              value='Stripe'
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check> */}
+            </Col>
+          </Form.Group>
+
+          <Button type="submit" variant="primary">
+            Continue
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
+}
